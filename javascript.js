96282217a55999383bc1a3034d1b97c9ccc12870
Mahdi Kahdouie <Mahdi.Kahdouie@gmail.com>
2026-05-28 13:36:17 +0330

    Step 2 is finished
    Write the logic to get the computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+function getComputerChoice(){
+    num=Math.floor(Math.random()*3)
+    console.log(num)
+    if (num==0) {
+        return "Rock"
+    }
+    else if (num==1){
+        return "Paper"
+    }
+    else if (num==2){
+        return "Scissors"
+    }
+}
