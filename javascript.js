c91a13e83135b921ca1c332bb0a6d809ec2d6a3d
Mahdi Kahdouie <Mahdi.Kahdouie@gmail.com>
2026-05-28 22:12:55 +0330

    Step 5 is finished
    Write the logic to play a single round

--- javascript.js
+++ javascript.js
@@ -1,23 +1,75 @@
 
-let humanScore = 0;
+let humanScore =0;
 let computerScore =0;
 
 function getComputerChoice(){
     num=Math.floor(Math.random()*3)
-    console.log(num)
     if (num==0) {
-        return "Rock"
+        return "rock"
     }
     else if (num==1){
-        return "Paper"
+        return "paper"
     }
     else if (num==2){
-        return "Scissors"
+        return "scissors"
     }
 }
 
 
 function getHumanChoice(){
    let choice= prompt("Enter your choice");
-   return choice;
+   return choice.toLowerCase();
 }
+
+
+
+
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice)
+    console.log(computerChoice)
+  switch (humanChoice) {
+    case "rock":
+        if (computerChoice=="paper"){
+            ++computerScore;
+            return console.log("You lose");
+            }
+        else if (computerChoice=="scissors"){
+            ++humanScore;
+            return console.log("You win");
+            }
+        else 
+            return  console.log("Draw");
+        break;
+
+    case "paper":
+        if (computerChoice=="scissors"){
+            ++computerScore;
+            return console.log("You lose");
+            }
+        else if (computerChoice=="rock"){
+            ++humanScore;
+            return console.log("You win");
+        }
+        else 
+            return  console.log("Draw");
+        break;
+
+    case "scissors":
+        if (computerChoice=="rock"){
+            ++computerScore;
+            return console.log("You lose");
+            }
+        else if (computerChoice=="paper"){
+            ++humanScore;
+            return console.log("You win");
+            }
+        else 
+            return  console.log("Draw");
+        break;
+  }
+
+
+}
+
+
+
